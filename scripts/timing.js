3cee06cc4ed8de27231960ba6529f4883aeb3ce2
Wichown <61947977+Wichown@users.noreply.github.com>
2023-04-29 19:50:22 -0600

    Update timing.js

--- scripts/timing.js
+++ scripts/timing.js
@@ -1,7 +1,7 @@
 //===
 // VARIABLES
 //===
-const DATE_TARGET = new Date('01/27/2023 0:01 AM');
+const DATE_TARGET = new Date('05/01/2023 0:01 AM');
 // DOM for render
 const SPAN_DAYS = document.querySelector('span#days');
 const SPAN_HOURS = document.querySelector('span#hours');
